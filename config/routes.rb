3190f7ca1124c513523c2dc9521da4ec29ddd0ee
Max Kern <m.kern@ingwiest.de>
2021-11-10 15:31:32 +0000

    Basis and Index set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # show all restaurants
+  # add a new restaurant
+  # read the details
+  # add a review
+  # all in one comment
+  resources :restaurants, only: [:index, :new, :show, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
